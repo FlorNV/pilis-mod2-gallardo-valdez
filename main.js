9117a37b95c39e661552f70cb979ff29132198f4
FlorNV <florencianoelivaldez@gmail.com>
2022-08-23 17:30:49 -0300

    Agrego mas estilos

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const navbar = document.getElementById("navbar");
 window.addEventListener("scroll", () => {
   if (window.scrollY > 10) {
     navbar.style.backgroundColor = "#FBF4E9";
+    navbar.style.backgroundColor = "#6ECB63";
   } else {
     navbar.style.backgroundColor = "transparent";
   }
